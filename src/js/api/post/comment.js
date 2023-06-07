3092497f3993628e6eb41786ff0e857ba2f10dbd
Fermain <Fermain@users.noreply.github.com>
2023-06-07 17:21:18 +0200

    Fixes method type

--- src/js/api/post/comment.js
+++ src/js/api/post/comment.js
@@ -3,5 +3,5 @@ import { fetchWrapper } from "../utilities/fetchWrapper.js";
 
 export async function commentOnPost(id = 0, body, replyToId) {
     if (!body) throw new Error("A body is required to make a comment");
-    return await fetchWrapper(API_POST_COMMENT(id), "PUT", {}, { body, replyToId })
+    return await fetchWrapper(API_POST_COMMENT(id), "POST", {}, { body, replyToId })
 }
