59d593e905f01a52ef228d86de6fbffedc253beb
Fermain <Fermain@users.noreply.github.com>
2023-06-07 14:46:27 +0200

    Adds outline for api options

--- src/js/api/post/list.js
+++ src/js/api/post/list.js
@@ -1,1 +1,19 @@
-export function listPosts(options) {}
+import { NAMESPACE } from "../../constants.js";
+
+export function listPosts(options = {
+    sort: "created",
+    sortOrder: "desc",
+    limit: 10,
+    offset: 0,
+    _tag: NAMESPACE,
+    _author: true,
+    _reactions: true,
+    _comments: true
+}, user) {
+
+    if (user) {
+        // Use profile/name/posts endpoint
+    } else {
+        // Use posts endpoint
+    }
+}
