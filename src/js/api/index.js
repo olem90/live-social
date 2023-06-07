9c1e2750b844c410abff540d4fe6dfebabf95a52
Fermain <Fermain@users.noreply.github.com>
2023-06-07 14:18:55 +0200

    Adds basic outline for api methods

---
+++ src/js/api/index.js
@@ -0,0 +1,5 @@
+export default class SocialAPI {
+    constructor(token = "") {
+        this.token = token;
+    }
+}
---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+import SocialAPI from "./api/index.js";
+
+const api = new SocialAPI();
+console.log(api);
