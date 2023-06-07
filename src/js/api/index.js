439b4a87bf7fac14bed84630b055f3050e0ce958
Fermain <Fermain@users.noreply.github.com>
2023-06-07 17:12:11 +0200

    Removes constructor

--- src/js/api/index.js
+++ src/js/api/index.js
@@ -3,10 +3,6 @@ import * as postMethods from "./post/index.js";
 import * as profileMethods from "./profile/index.js";
 
 export default class SocialAPI {
-    constructor(token = "") {
-        this.token = token;
-    }
-
     auth = authMethods
     post = postMethods
     profile = profileMethods
