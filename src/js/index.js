e9fd3cd6f1ff262fbafd13c647441c4f5014c9e6
Fermain <Fermain@users.noreply.github.com>
2023-06-07 16:30:14 +0200

    Adds test routine

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,14 @@ import SocialAPI from "./api/index.js";
 
 const api = new SocialAPI();
 console.log(api);
+
+async function testRoutine() {
+    let post = await api.post.create({title: "test"})
+    console.log(post);
+    post = await api.post.update(post.id, {
+        title: "updated"
+    });
+
+    console.log(post.title);
+}
+
