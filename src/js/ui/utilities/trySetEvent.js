c85e40055f15ef9fa7ae89c6dab03861c81927fe
Fermain <Fermain@users.noreply.github.com>
2023-06-08 18:26:19 +0200

    Fixes minor bug

--- src/js/ui/utilities/trySetEvent.js
+++ src/js/ui/utilities/trySetEvent.js
@@ -1,6 +1,5 @@
 export function trySetEvent(selector, eventType, listener = () => {}) {
     const targets = document.querySelectorAll(selector);
-    console.log(targets, selector);
     targets.forEach(target => {
         target.addEventListener(eventType, listener)
     })
