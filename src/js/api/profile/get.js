9a576aefc8c9d107113fef89a9fc4e38a98c834d
Fermain <Fermain@users.noreply.github.com>
2023-06-08 21:38:49 +0200

    Improves template logic

--- src/js/api/profile/get.js
+++ src/js/api/profile/get.js
@@ -1,6 +1,7 @@
 import { API_PROFILE, DEFAULT_PROFILE_PARAMS } from "../../constants.js";
 import { fetchWrapper } from "../utilities/fetchWrapper.js";
 
-export async function getProfile(id = 0) {
-    return await fetchWrapper(API_PROFILE(id), "GET", DEFAULT_PROFILE_PARAMS)
+export async function getProfile(name = "", params = DEFAULT_PROFILE_PARAMS) {
+    if (!name) throw new Error("Profile name is required");
+    return await fetchWrapper(API_PROFILE(name), "GET", params)
 }
--- src/js/routes/profile.js
+++ src/js/routes/profile.js
@@ -1,4 +1,6 @@
+import { getProfile } from "../api/profile/get.js";
 import { me } from "../api/utilities/index.js"
+import { DEFAULT_PROFILE_PARAMS } from "../constants.js";
 import { authGuard } from "../router.js";
 import { loadTemplate } from "../template.js";
 
@@ -13,4 +15,13 @@ export async function profileRoute(name) {
     }
 
     loadTemplate(template, user)
+
+    const { posts } = await getProfile(name, {
+        ...DEFAULT_PROFILE_PARAMS,
+        _posts: true
+    });
+
+    posts.forEach(post => {
+        loadTemplate("post/list", { ...post}, document.querySelector(".posts"), false);
+    })
 }
