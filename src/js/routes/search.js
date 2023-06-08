addb0abd31f5c085d77230d113ef623c2826b295
Fermain <Fermain@users.noreply.github.com>
2023-06-08 20:50:07 +0200

    Adds search implementation

--- src/js/routes/search.js
+++ src/js/routes/search.js
@@ -1,7 +1,12 @@
 import { loadTemplate } from "../template.js";
+import { search } from "../ui/search.js";
 
 export async function searchRoute(query) {
     const template = "search";
     await loadTemplate(template, { query });
-    console.log(`<Search query="${query}">`);
+    const results = await search(query);
+    results.map(post => {
+        loadTemplate("post", { ...post }, document.querySelector(".results"), false)
+    })
+    document.querySelector(".search .spinner-grow").classList.add("d-none");
 }
---
+++ src/js/ui/search.js
@@ -0,0 +1,25 @@
+import { listPosts } from "../api/post/list.js";
+
+export async function search(query) {
+    const posts = await listPosts({
+        limit: 100
+    });
+
+    return searchPosts(query, posts);
+}
+
+export function searchPosts(query, posts) {
+    function criteria(post = {
+        title: "",
+        tags: [""],
+        body: ""
+    }) {
+        const titleMatches = post.title.includes(query);
+        const tagsMatch = post.tags.includes(query);
+        const bodyMatches = post.body && post.body.includes(query);
+        return titleMatches || tagsMatch || bodyMatches;
+    }
+
+    return posts.filter(criteria)
+}
+
