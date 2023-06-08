dee99a5cddfb84fcecf8c901566b9e60c8ec3883
Fermain <Fermain@users.noreply.github.com>
2023-06-08 18:35:51 +0200

    Changes content div to main

--- src/js/template.js
+++ src/js/template.js
@@ -1,6 +1,7 @@
 import { ui } from "./ui/index.js";
 
-export async function loadTemplate(name = "notFound", variables = {}, target = document.body) {
+export async function loadTemplate(name = "notFound", variables = {}, target = document.body.querySelector("main")) {
+    if (!target) throw new Error("Target not found in the document");
     const response = await fetch(`/src/html/${name}.html`);
     const html = await response.text();
     target.innerHTML = replaceTemplateVariables(html, {...variables});
