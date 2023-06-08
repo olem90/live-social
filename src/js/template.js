012bd3f508fa10b52431759af69c1885a4b4363b
Fermain <Fermain@users.noreply.github.com>
2023-06-08 20:49:36 +0200

    Improves clearing strat

--- src/js/template.js
+++ src/js/template.js
@@ -1,12 +1,17 @@
 import { ui } from "./ui/index.js";
+import { clearChildren } from "./ui/utilities/clearChildren.js";
 import { parse } from "./ui/utilities/parse.js";
 
-export async function loadTemplate(name = "notFound", variables = {}, target = document.body.querySelector("main")) {
+export async function loadTemplate(name = "notFound", variables = {}, target = document.body.querySelector("main"), replace = true) {
     if (!target) throw new Error("Target not found in the document");
     const response = await fetch(`/src/html/${name}.html`);
     const template = await response.text();
     const html = parse(replaceTemplateVariables(template, {...variables}));
-    target.innerHTML = "";
+    
+    if (replace) {
+        clearChildren(target)
+    }
+
     target.append(html)
     ui()
 }
---
+++ src/js/ui/utilities/clearChildren.js
@@ -0,0 +1,5 @@
+export function clearChildren(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
