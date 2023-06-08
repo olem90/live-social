9dbbc1809e69fc74292e29a9e2060ef60e32f103
Fermain <Fermain@users.noreply.github.com>
2023-06-08 18:22:32 +0200

    Adds button loader

--- src/js/ui/events/onLogin.js
+++ src/js/ui/events/onLogin.js
@@ -1,9 +1,12 @@
 import { formHandler } from "../utilities/index.js";
 import { login } from "../../api/auth/login.js";
 import { redirect } from "../../router.js";
+import { buttonLoader } from "../utilities/buttonLoader.js";
 
 export async function onLogin(event = new SubmitEvent()) {
     const { email, password } = formHandler(event);
+    const submit = event.target.querySelector("button[type=submit]");
+    buttonLoader(submit, "Logging in...")
     await login(email, password);
     redirect("/profile");
 }
--- src/js/ui/events/onRegister.js
+++ src/js/ui/events/onRegister.js
@@ -1,9 +1,12 @@
 import { formHandler } from "../utilities/index.js";
 import { register } from "../../api/auth/register.js";
 import { redirect } from "../../router.js";
+import { buttonLoader } from "../utilities/buttonLoader.js";
 
 export async function onRegister(event = new SubmitEvent()) {
     const { email, password, name, banner, avatar } = formHandler(event);
+    const submit = event.target.querySelector("button[type=submit]");
+    buttonLoader(submit, "Registering...");
     await register(email, password, name, avatar, banner);
     redirect("/login");
 }
