9ab67f6c7537bfb55adc57170f01e7031b492543
Fermain <Fermain@users.noreply.github.com>
2023-06-10 15:38:00 +0200

    Implements nearest ancestor for navigation

--- src/js/ui/events/onNavigate.js
+++ src/js/ui/events/onNavigate.js
@@ -1,9 +1,12 @@
 import { redirect } from "../../router.js";
+import { nearestAncestor } from "../utilities/nearestAncestor.js";
 
 export function onNavigate(event = new MouseEvent()) {
-    if (event.target.tagName === "A") {
-        const url = new URL(event.target.href);
+    const target = nearestAncestor(event.target);
+    if (target) {
+        const url = new URL(target.href);
         if (url.hostname === location.hostname) {
+            console.log("Navigate");
             event.preventDefault();
             redirect(url.pathname + url.search)
         }
