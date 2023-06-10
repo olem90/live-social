4ee944539565dfdd1a58a75a24ca528936e3bcf9
Fermain <Fermain@users.noreply.github.com>
2023-06-10 16:42:39 +0200

    Adds array key support

--- src/js/ui/utilities/formHandler.js
+++ src/js/ui/utilities/formHandler.js
@@ -3,5 +3,15 @@ export function formHandler(event = new SubmitEvent()) {
     const form = event.target;
     const formData = new FormData(form);
     const data = Object.fromEntries(formData.entries());
+    Object.keys(data).filter(key => key.charAt(0) === "[" && key.charAt(key.length - 1) === "]").map(key => {
+        return {
+            oldKey: key,
+            newKey: key.replace(/[\[\]]/gm, ""),
+            values: formData.getAll(key).filter(Boolean)
+        }
+    }).forEach(({ oldKey, newKey, values }) => {
+        delete data[oldKey];
+        data[newKey] = values;
+    })
     return data;
 }
