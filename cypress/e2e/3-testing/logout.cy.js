0514d8ce1a953c206c03894388dee8e40c3beb4d
olem90 <olemariusrognan90@gmail.com>
2023-06-17 20:54:54 +0100

    modiffied logout.cy.js

--- cypress/e2e/3-testing/logout.cy.js
+++ cypress/e2e/3-testing/logout.cy.js
@@ -4,12 +4,21 @@ describe('logout', () => {
       cy.visit('/src/html/login.html');
       cy.wait(2000);
 
+      cy.intercept('GET', 'https://api.noroff.dev/api/v1/social/posts*', {
+        statusCode: 200,  
+        body: []  
+});
+
       cy.get('.login #email').clear().type('testuser123456@stud.noroff.no');
       cy.get('.login #password').clear().type('testuser123456{enter}');
 
+      cy.url().should('include', '/profile');
+
       cy.wait(4000);
       
-      cy.visit("/index.html");
+      cy.visit("/");
+
+      cy.wait(5000)
       cy.get('.logout').click();
 
       cy.wait(10000)
