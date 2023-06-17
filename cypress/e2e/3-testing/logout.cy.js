ffe3b6bd775e6a6e924bb9f39e465938ff0c1eb4
olem90 <olemariusrognan90@gmail.com>
2023-06-17 20:35:12 +0100

    changed logout.cy.js file

--- cypress/e2e/3-testing/logout.cy.js
+++ cypress/e2e/3-testing/logout.cy.js
@@ -1,8 +1,15 @@
 describe('logout', () => {
     it('successfully logs out user with logout button', () => {
       
-      cy.visit('http://127.0.0.1:5500/index.html');
+      cy.visit('/src/html/login.html');
+      cy.wait(2000);
 
+      cy.get('.login #email').clear().type('testuser123456@stud.noroff.no');
+      cy.get('.login #password').clear().type('testuser123456{enter}');
+
+      cy.wait(4000);
+      
+      cy.visit("/index.html");
       cy.get('.logout').click();
 
       cy.wait(10000)
